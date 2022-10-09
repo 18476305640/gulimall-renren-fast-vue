2b5a4c2a90091f59ab1303feb68dd3ae571161df
manZhungjie <63949859+18476305640@users.noreply.github.com>
2022-10-09 22:46:21 +0800

    '添加商品完成'

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import '@/assets/scss/index.scss'
 import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
 import { isAuth } from '@/utils'
 import cloneDeep from 'lodash/cloneDeep'
+import PubSub from 'pubsub-js'
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
@@ -25,6 +26,7 @@ Vue.prototype.isAuth = isAuth     // 权限方法
 
 // 保存整站vuex本地储存初始状态
 window.SITE_CONFIG['storeState'] = cloneDeep(store.state)
+Vue.prototype.PubSub = PubSub
 
 /* eslint-disable no-new */
 new Vue({
